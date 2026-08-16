5fb9d0edc83e04ed8a72aa9144b56f1d6fb5b007
Shawn <shawndullen@hotmail.com>
2026-08-16 01:43:07 -0400

    Load commerce experience styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './styles/global.css';
+import './styles/commerce.css';
 
 const root = document.getElementById('root');
 if (!root) throw new Error('Root element was not found.');
