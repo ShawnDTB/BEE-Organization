2f9675a381ed70964a4d6341f70925ab494c4d0d
Shawn <shawndullen@hotmail.com>
2026-08-16 02:17:55 -0400

    Load storefront Studio and density styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,9 @@ import App from './App';
 import './styles/global.css';
 import './styles/commerce.css';
 import './styles/commerce-polish.css';
+import './styles/shop-v2.css';
+import './styles/studio.css';
+import './styles/density.css';
 
 const root = document.getElementById('root');
 if (!root) throw new Error('Root element was not found.');
