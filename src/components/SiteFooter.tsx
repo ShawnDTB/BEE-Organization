513f86b3fae5032f3701c59660246b8d25716b83
Shawn <shawndullen@hotmail.com>
2026-08-16 22:28:21 -0400

    Surface group collector from footer

--- src/components/SiteFooter.tsx
+++ src/components/SiteFooter.tsx
@@ -10,7 +10,7 @@ export function SiteFooter() {
       </div>
 
       <div className="site-footer__grid site-footer__grid--v3">
-        <div><h2>Start</h2><a href="/studio">BEE Studio</a><a href="/bulk-orders">Bulk & organizations</a><a href="/creator-merch">Creator merch</a><a href="/shop">Shop collections</a></div>
+        <div><h2>Start</h2><a href="/studio">BEE Studio</a><a href="/bulk-orders">Bulk & organizations</a><a href="/group-collector">Group size collector</a><a href="/creator-merch">Creator merch</a><a href="/shop">Shop collections</a></div>
         <div><h2>Capabilities</h2><a href="/embroidery">Embroidery</a><a href="/graphic-apparel">Graphic apparel</a><a href="/our-work">Sample Lab / Work</a></div>
         <div><h2>Your project</h2><a href="/cart">Project bag</a><a href="/project-review">Project review</a><a href="/account">Customer workspace</a><a href="/about">About BEE</a></div>
       </div>
